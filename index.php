375ec3a3642febd298c0efc9c96f76431b55996e
Philip Tellis <philip@bluesmoon.info>
2010-11-05 14:43:32 -0700

    support files for latex talks

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<title>Sometimes I talk</title>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<link rel="shortcut icon" href="http://i1.bluesmoon.info/favicon.ico" type="image/x-icon">
+<link rel="openid.server" href="http://www.livejournal.com/openid/server.bml">
+<link rel="openid.delegate" href="http://bluesmoon.livejournal.com/">
+
+<link rel="pgpkeys" type="application/pgp-keys" title="Philip S Tellis's GPG Public Key" href="http://bluesmoon.info/bluesmoon.asc">
+<link rel="pgpkeys" type="application/pgp-keys" title="Philip S Tellis's GPG Public Key (Key server)" href="http://pgpkeys.mit.edu:11371/pks/lookup?op=get&amp;search=0x1F140E17">
+
+<link rel="stylesheet" type="text/css" href="http://a1.bluesmoon.info/blue.css">
+<?php
+function get_url($url, $filter=null)
+{
+	if(empty($url))
+		return null;
+
+	$hash = strftime("%Y-%m-%d-") . md5($url);
+	$file = "/home/ptellis/cache/$hash";
+	if(file_exists($file)) {
+		return file_get_contents($file);
+	}
+
+	$str = file_get_contents($url);
+	if($filter) {
+		$str = call_user_func($filter, $str);
+	}
+	file_put_contents($file, $str, LOCK_EX);
+
+	return $str;
+}
+
+function extract_talks_from_github($html)
+{
+	$d = @DOMDocument::loadHTML($html);
+
+	$x = new DOMXPath($d);
+	$nodes = $x->query("//li/a[1]");
+
+	$talks = array();
+	foreach($nodes as $node) {
+		foreach($node->attributes as $a) {
+			if($a->name == 'href') {
+				$talks[] = array('url' => "http://bluesmoon.github.com/talks/" . $a->value, 'name' => $node->nodeValue);
+				break;
+			}
+		}
+	}
+
+	return json_encode($talks);
+}
+
+
+function get_talks()
+{
+	include('talks.php');
+	$github_talks = json_decode(get_url("http://bluesmoon.github.com/talks/", "extract_talks_from_github"), true);
+
+	$t = array_merge($talks, $github_talks);
+
+	$talks = array();
+	foreach($t as $talk)
+		$talks[strtolower($talk['name'])] = $talk;
+
+	ksort($talks);
+	return $talks;
+}
+	
+?>
+
+</head>
+<body class="talks wide">
+
+<div id="content">
+<?php include "/home/ptellis/templates/header.html" ?>
+ <div class="vcard">
+  <h1>Sometimes <em><strong>blues</strong> talks</em></h1>
+  <p>
+   <img id="avatar" class="photo" src="http://img.avatars.yahoo.com/users/1xr7bvyKNAAIC_EF0AJbtg4wA.64.png" height="64" width="64" alt="[philiptellis]">
+   I speak at conferences around the world on several topics including the spirit of the hack, web performance, opensource software, project management and everything in between.
+   I'd love to come speak at your conference if you'll have me.
+  </p>
+  <br>
+ </div> <!-- end vcard -->
+
+ <div id="body">
+  <div id="bodycontent">
+<?php
+$talks = get_talks();
+
+if(isset($_GET['talk']) && array_key_exists(strtolower($_GET['talk']), $talks)) {
+	$selected_talk = $talks[strtolower($_GET['talk'])];
+?>
+<div style="text-align: left;">
+<h2><?php echo htmlentities($selected_talk['name']); ?></h2>
+<?php
+	if(isset($selected_talk['slideshare'])) {
+?>
+<div style="width:640px;text-align:left" id="<?php echo $selected_talk['slideshare']['id'] ?>">
+  <object style="margin:0px" width="640" height="533">
+    <param name="movie" value="http://static.slidesharecdn.com/swf/ssplayer2.swf?rel=0&doc=<?php echo urlencode($selected_talk['slideshare']['file']) ?>&stripped_title=<?php echo urlencode($selected_talk['slideshare']['title']); ?>" />
+    <param name="allowFullScreen" value="true"/>
+    <param name="allowScriptAccess" value="always"/>
+    <embed src="http://static.slidesharecdn.com/swf/ssplayer2.swf?rel=0&doc=<?php echo urlencode($selected_talk['slideshare']['file']) ?>&stripped_title=<?php echo urlencode($selected_talk['slideshare']['title']); ?>" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="640" height="533"></embed>
+  </object>
+  <a href="http://www.slideshare.net/bluesmoon/<?php echo $selected_talk['slideshare']['title'] ?>#postComment">Comment on SlideShare</a> | <a href="<?php echo $selected_talk['source']?>">View Source</a>.
+</div>
+<?php
+	}
+	else {
+?>
+<a href="<?php echo $selected_talk['url'] ?>" target="_blank">Open in new window</a>
+<iframe src="<?php echo $selected_talk['url'] ?>" style="height:600px; width:800px;"></iframe>
+<?php
+	}
+?>
+</div>
+<?php
+}
+else {
+?>
+<h2>Hacking my talks</h2>
+<p>
+I've been lecturing at universities and speaking at conferences and user group events for a little over 10 years now.  At the start I used to write raw HTML by hand using frames to keep navigation on the left.  This worked well because if the machine I was using to present didn't have the required tools installed, I could ssh in to my box and do the presentation using lynx.  Many years later, Eric Meyer developed S5 and I switched to using that.  I wrote a custom perl script that generated the S5 file from a plain text file.  The perl script is available on my github account <a href="http://github.com/bluesmoon/talks">here</a>.
+</p>
+<p>
+Late in 2009, in preparation for FOSS.IN, I decided to start learning LaTeX and build my presentations with that, then generate a PDF with pdflatex.  The primary motivation was to be able to upload my presentations to slideshare.  I first redid Being a Geek as a dry run.  At FOSS.IN, I got a bunch of help from other LaTeX experts to build my talk.  I haven't mastered it yet, but I'm getting there.
+</p>
+<p>
+Click on any of the links on the right and they should show up here.
+</p>
+<?php
+}
+?>
+  </div>
+ </div> <!-- end body -->
+
+ <div id="sidebar2">
+<div class="repo">
+<h2>Talks</h2>
+<ul>
+<?php
+foreach($talks as $n => $talk) {
+?>
+ <li><a href="http://talks.bluesmoon.info/?talk=<?php echo urlencode($talk['name']) ?>"><?php echo htmlentities($talk['name'], ENT_QUOTES); ?></a></li>
+<?php
+}
+?>
+</ul>
+</div>
+
+ </div> <!-- end sidebar2 -->
+
+
+ <div id="footer">
+
+  <ul>
+   <li class="feed"><a title="Flickr photostream" href="http://api.flickr.com/services/feeds/photos_public.gne?id=57155801@N00&amp;lang=en-us&amp;format=rss_200">PHOTO FEED</a></li>
+   <li><a title="Validate this page's markup" href="http://validator.w3.org/check?uri=referer">HTML 4.01</a></li>
+   <li><a title="Validate this page's CSS" href="http://jigsaw.w3.org/css-validator/check/referrer">CSS 2.1</a></li>
+   <li>&copy; PHILIP TELLIS 2009</li>
+  </ul>
+
+ </div> <!-- end footer -->
+
+</div> <!-- end content -->
+
+<script type="text/javascript" src="http://a1.bluesmoon.info/piwik.js"></script><script type="text/javascript">try { var piwikTracker = Piwik.getTracker("http://bluesmoon.org/analytics/piwik/piwik.php", 1); piwikTracker.trackPageView(); piwikTracker.enableLinkTracking(); } catch( err ) {}</script><noscript><p><img src="http://bluesmoon.org/analytics/piwik/piwik.php?idsite=1" style="border:0;position:absolute;left:-9999em;" alt=""></p></noscript>
+<script type="text/javascript" src="http://p.bluesmoon.info/boomerang/boomerang.js"></script>
+<script type="text/javascript">
+BOOMR.init({
+		beacon_url: "http://bluesmoon.org/analytics/boomerang/b.php",
+		site_domain: "bluesmoon.info",
+		log: null,
+		user_ip: "<?php echo $_SERVER['REMOTE_ADDR'] ?>",
+		BW: {
+			base_url: "http://p.bluesmoon.info/boomerang/images/"
+		}
+}).addVar({ pg: 1});
+</script>
+</body>
+</html>
---
+++ talks.php
@@ -0,0 +1,99 @@
+<?php
+$talks = array(
+		array(
+			'name' => 'shut up and hack',
+			'slideshare' => array('id' => '__ss_2651520', 'title' => 'shut-up-and-hack', 'file' => 'shut-up-and-hack-091204141739-phpapp02'),
+			'source' => 'http://talks.bluesmoon.info/shut-up-and-hack/index.tex',
+			'date' => '2009-12-04 18:00'
+		),
+		array(
+			'name' => 'being a geek',
+			'slideshare' => array('id' => '__ss_2551535', 'title' => 'being-a-geek', 'file'=> 'index-091121041214-phpapp02'),
+			'source' => 'http://talks.bluesmoon.info/being_a_geek/index.txt',
+			'date' => '2004-12-03 16:00'
+		),
+		array(
+			'name' => 'Project management with bugzilla, CVS and mailing lists',
+			'url' => 'http://talks.bluesmoon.info/ubuntulive-bugzilla_cvs/',
+			'date' => '2004-12-03 12:00'
+		),
+		array(
+			'name' => 'Fallback Messaging',
+			'slideshare' => array('id' => '__ss_2706240', 'title' => 'fallback-messaging', 'file'=> 'fallbackmessaging-091212145838-phpapp01'),
+			'url' => 'http://talks.bluesmoon.info/fallback_messaging/',
+			'source' => 'http://talks.bluesmoon.info/fallback_messaging/fallback_messaging.txt',
+			'date' => '2004-12-02 14:00'
+		),
+		array(
+			'name' => 'Why FOSS in Education makes sense',
+			'slideshare' => array('id' => '__ss_2707852', 'title' => 'why-foss-in-education-makes-sense', 'file'=> 'fosseducation-091213042643-phpapp01'),
+			'url' => 'http://talks.bluesmoon.info/foss-education/',
+			'source' => 'http://talks.bluesmoon.info/foss-education/foss_education.txt',
+			'date' => '2005-12-01 10:30'
+		),
+		array(
+			'name' => 'Microformats',
+			'url' => 'http://talks.bluesmoon.info/microformats/',
+			'date' => '2007-02-23'
+		),
+		array(
+			'name' => 'Regular Expressions Demystified',
+			'slideshare' => array('id' => '__ss_2535771', 'title' => 'regular-expressions-demystified', 'file' => 'regex-demystified-091119061907-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/regex-demystified/regex-demystified.tex'
+		),
+		array(
+			'name' => 'Javascript charting with YUI Flot',
+			'slideshare' => array('id' => '__ss_3095493', 'title' => 'javascript-charting-with-yuiflot', 'file' => 'fosdem2010-100207063007-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/yui-flot/fosdem2010.tex',
+			'date' => '2010-02-07 12:00'
+		),
+		array(
+			'name' => 'Scaling writes through MySQL partitioning (Montreal)',
+			'slideshare' => array('id' => '__ss_3397422', 'title' => 'scaling-mysql-writes-through-partitioning-3397422', 'file' => 'index-100311083201-phpapp02'),
+			'source' => 'http://talks.bluesmoon.info/scaling-mysql-writes/index.tex',
+			'date' => '2010-03-10 15:00'
+		),
+		array(
+			'name' => 'The Statistics of Web Performance',
+			'slideshare' => array('id' => '__ss_3441823', 'title' => 'index-3441823', 'file' => 'index-100315192839-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/statistics-of-performance-measurement/index.tex',
+			'date' => '2010-03-12 08:30'
+		),
+		array(
+			'name' => 'Websites on Overdrive',
+			'slideshare' => array('id' => '__ss_3986222', 'title' => 'websites-on-overdrive', 'file' => 'websites-on-overdrive-100506003339-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/websites-on-overdrive/websites-on-overdrive.tex',
+			'date' => '2010-05-06 14:00'
+		),
+		array(
+			'name' => 'YQL: Find more than just data',
+			'slideshare' => array('id' => '__ss_4046229', 'title' => 'missing-kids-on-your-404-page', 'file' => 'missing-kids-100511020614-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/missing-kids/missing-kids.tex',
+			'date' => '2010-05-11 19:00'
+		),
+		array(
+			'name' => 'Station-wagons and electrons',
+			'slideshare' => array('id' => '__ss_4333433', 'title' => 'stationwagons-and-electrons', 'file' => 'station-wagons-and-electrons-100527162453-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/station-wagons-and-electrons/station-wagons-and-electrons.tex',
+			'date' => '2010-05-27 19:00'
+		),
+		array(
+			'name' => 'Scaling MySQL writes through Partitioning (Berlin)',
+			'slideshare' => array('id' => '__ss_4393022', 'title' => 'scaling-mysql-writes-through-partitioning-ipc-spring-edition', 'file' => 'ipc-spring-100602135311-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/scaling-mysql-writes/ipc-spring.tex',
+			'date' => '2010-06-01 14:00'
+		),
+		array(
+			'name' => 'Latency: Why you should worry, what you can do about it',
+			'slideshare' => array('id' => '__ss_4609886', 'title' => 'latency-why-you-should-worry-what-you-can-do-about-it', 'file' => 'latency-100625024834-phpapp01'),
+			'source' => 'http://talks.bluesmoon.info/velocity2010/latency.tex',
+			'date' => '2010-06-24 16:48'
+		),
+		array(
+			'name' => 'Measuring the web with boomerang',
+			'slideshare' => array('id' => '__ss_4951943', 'file' => 'boomerang-100812041053-phpapp01', 'title' => 'measuring-the-web-with-boomerang'),
+			'source' => 'http://talks.bluesmoon.info/sf-web-perf-boomerang/boomerang.tex',
+			'date' => '2010-08-11 19:00'
+		)
+	);
+?>
